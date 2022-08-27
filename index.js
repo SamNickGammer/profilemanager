5faff8ad212f83d61863882197c23a9786e0b098
Sam Nick <72904070+SamNickGammer@users.noreply.github.com>
2022-08-27 19:08:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const app = require('./app');
 const http = require('http');
+const cors = require('cors');
 const { PORT } = require('./utils/config');
 const connectToDB = require('./db');
+
 
 connectToDB();
 
 const server = http.createServer(app);
-
+app.use(cors());
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('/client/build'));
 // }
